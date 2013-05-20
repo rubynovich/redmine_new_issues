1e9a4036a40ecf7a7e0f7bdc31f7b91395f233de
rubynovich <roman@shipiev.me>
2013-05-20 15:29:32 +0400

    fix: github -> bitbucket

--- init.rb
+++ init.rb
@@ -1,11 +1,11 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_new_issues do
-  name 'Redmine New Issues plugin'
+  name 'New Issues'
   author 'Roman Shipiev'
   description 'Block for my/page and rake-task for mail delivery (IssueStatus.default)'
   version '0.0.1'
-  url 'https://github.com/rubynovich/redmine_new_issues'
+  url 'https://bitbucket.org/rubynovich/redmine_new_issues'
   author_url 'http://roman.shipiev.me'
 end
 
