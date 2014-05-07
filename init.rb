25cc09d380f26da34d6322b90ba3e30061ed6fb8
Maksim <max@koritskiy.ru>
2014-05-07 17:06:22 +0400

    add support redmine 2.5 and over

--- init.rb
+++ init.rb
@@ -4,8 +4,8 @@ Redmine::Plugin.register :redmine_new_issues do
   name 'New Issues'
   author 'Roman Shipiev'
   description 'Block for my/page and rake-task for mail delivery (IssueStatus.default)'
-  version '0.0.1'
-  url 'https://bitbucket.org/rubynovich/redmine_new_issues'
+  version '0.0.2'
+  url 'https://bitbucket.org/gorkapstroy/redmine_new_issues'
   author_url 'http://roman.shipiev.me'
 end
 
@@ -17,13 +17,13 @@ else
 end
 
 object_to_prepare.to_prepare do
-  [:mailer, :issue].each do |cl|
+  [:mailer,  (Redmine::VERSION::MAJOR >= 2 && Redmine::VERSION::MINOR >= 5) ? :issue : :issue_observer].each do |cl|
     require "new_issues_#{cl}_patch"
   end
 
   [
     [Mailer, NewIssuesPlugin::MailerPatch],
-    [Issue, NewIssuesPlugin::IssuePatch]
+    ((Redmine::VERSION::MAJOR >= 2 && Redmine::VERSION::MINOR >= 5) ? [Issue, NewIssuesPlugin::IssuePatch] : [IssueObserver, NewIssuesPlugin::IssueObserverPatch])
   ].each do |cl, patch|
     cl.send(:include, patch) unless cl.included_modules.include? patch
   end
---
+++ lib/new_issues_issue_observer_patch.rb
@@ -0,0 +1,24 @@
+require_dependency 'issue_observer'
+
+module NewIssuesPlugin
+  module IssueObserverPatch
+    def self.included(base)
+      base.extend(ClassMethods)
+
+      base.send(:include, InstanceMethods)
+
+      base.class_eval do
+        alias_method_chain :after_create, :new_issues
+      end
+    end
+
+    module ClassMethods
+    end
+
+    module InstanceMethods
+      def after_create_with_new_issues(issue)
+        after_create_without_new_issues(issue) if issue.start_date && (issue.start_date <= Date.today)
+      end
+    end
+  end
+end
